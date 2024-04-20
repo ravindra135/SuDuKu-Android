291910da74ce1d29d27efbe211bae3c5db368d9b
Ravindra Reddy <reddyravindra947@gmail.com>
2024-04-20 14:57:28 +0530

    upd: Action Buttons
    - 20 April 2024
    - Action button work done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-//    implementation(libs.androidx.databinding.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/rcube/suduku/MainActivity.kt
+++ app/src/main/java/com/rcube/suduku/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.rcube.suduku
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -9,23 +12,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
 import com.rcube.suduku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var selectedNumber: String? = null;
-    
+    private var selectedNumber: String? = null
+    private var selectedButton: View? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 //        This is used when your xml root is not layout;
-//        binding = ActivityMainBinding.inflate(layoutInflater);
-//        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
 
 //        When your xml root is layout
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
@@ -41,12 +44,44 @@ class MainActivity : AppCompatActivity() {
 
     fun actionBtnClicked(view: View) {
         if(view is Button) {
+
+            toggleBtnState()
+
+            selectedButton = view
             selectedNumber = view.text.toString()
+
+            view.setTextColor(binding.root.context.getColor(R.color.white))
+            view.background = binding.root.context.getDrawable(R.drawable.rounded_button_pressed)
+
             Toast.makeText(this, "$selectedNumber selected", Toast.LENGTH_SHORT).show()
         }
         
         if(view is ImageButton) {
+            toggleBtnState()
+
+            selectedNumber = ""
+            selectedButton = view
+
+            val drawable = view.drawable as VectorDrawable
+            drawable.setTintList(ColorStateList.valueOf(Color.WHITE))
+            view.background = binding.root.context.getDrawable(R.drawable.rounded_button_pressed)
+
             Toast.makeText(this, "Delete option selected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun toggleBtnState() {
+        if (selectedButton!= null) {
+            if (selectedButton is Button) {
+                (selectedButton as Button).background = binding.root.context.getDrawable(R.drawable.rounded_button)
+                (selectedButton as Button).setTextColor(binding.root.context.getColor(R.color.black))
+            } else if (selectedButton is ImageButton) {
+                val imageButton = selectedButton as ImageButton
+                imageButton.background = binding.root.context.getDrawable(R.drawable.rounded_button)
+                val drawable = imageButton.drawable as VectorDrawable
+                drawable.setTintList(ColorStateList.valueOf(Color.BLACK))
+            }
+            selectedButton = null
         }
     }
 }
