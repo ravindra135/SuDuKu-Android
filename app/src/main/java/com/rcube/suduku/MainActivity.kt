61c9f780d66fdf1a972b330e539dea36f4b3b729
Ravindra Reddy <reddyravindra947@gmail.com>
2024-05-05 13:53:16 +0530

    upd: SuDuKu Grid Box, click work done
    - 05 May 2024

--- app/src/main/java/com/rcube/suduku/MainActivity.kt
+++ app/src/main/java/com/rcube/suduku/MainActivity.kt
@@ -1,151 +1,41 @@
 package com.rcube.suduku
 
-import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
-import com.rcube.suduku.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), SudokuGridLayout.OnCellTouch {
-
-    private lateinit var binding: ActivityMainBinding
-    private var selectedNumber: String? = null
-    private var selectedButton: View? = null
-
-    private lateinit var SudukuGridLayout: SudokuGridLayout
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        This is used when your xml root is not layout;
-        binding = ActivityMainBinding.inflate(layoutInflater);
-        setContentView(binding.root)
-
-//        When your xml root is layout
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_main)
 
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+
+        val mainLayout = findViewById<View>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-//        Call API for new game
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://sudoku-api.vercel.app/api/dosuku"
-
-        val jsonObjReq = JsonObjectRequest(Request.Method.GET, url, null,
-            { response ->
-
-                val gridValue = response.getJSONObject("newboard").getJSONArray("grids").getJSONObject(0).getJSONArray("value")
-                val solValue = response.getJSONObject("newboard").getJSONArray("grids").getJSONObject(0).getJSONArray("solution")
-
-                val grid = Array(9) { IntArray(9) }
-
-                for(i in 0 until 9) {
-                    val row = gridValue.getJSONArray(i)
-                    val sol = solValue.getJSONArray(i)
-
-                    Log.i("SuDuKu", "Value of $i: $row")
-                    Log.i("SuDuKu", "Solution of $i: $sol")
-                }
-                Toast.makeText(this, "Game Started", Toast.LENGTH_SHORT).show()
-
-            },
-            { error ->
-                Log.e("SuDuKu", "Error: $error")
-                Toast.makeText(this, "Cant Fetch Game Data", Toast.LENGTH_SHORT).show()
-            }
-        )
-
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjReq)
+        Log.i("SuDuKu_Game", "Game Created")
+        Toast.makeText(this, "Game Created", Toast.LENGTH_SHORT).show()
     }
 
+    // Button Clicked
     fun actionBtnClicked(view: View) {
-        if(view is Button) {
-
-            toggleBtnState()
-
-            selectedButton = view
-            selectedNumber = view.text.toString()
-
-            view.setTextColor(binding.root.context.getColor(R.color.white))
-            view.background = binding.root.context.getDrawable(R.drawable.rounded_button_pressed)
-
-//            Toast.makeText(this, "$selectedNumber selected", Toast.LENGTH_SHORT).show()
-        }
-        
-        if(view is ImageButton) {
-            toggleBtnState()
-
-            selectedNumber = ""
-            selectedButton = view
-
-            val drawable = view.drawable as VectorDrawable
-            drawable.setTintList(ColorStateList.valueOf(Color.WHITE))
-            view.background = binding.root.context.getDrawable(R.drawable.rounded_button_pressed)
-
-//            Toast.makeText(this, "Delete option selected", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun toggleBtnState() {
-        if (selectedButton!= null) {
-            if (selectedButton is Button) {
-                (selectedButton as Button).background = binding.root.context.getDrawable(R.drawable.rounded_button)
-                (selectedButton as Button).setTextColor(binding.root.context.getColor(R.color.black))
-            } else if (selectedButton is ImageButton) {
-                val imageButton = selectedButton as ImageButton
-                imageButton.background = binding.root.context.getDrawable(R.drawable.rounded_button)
-                val drawable = imageButton.drawable as VectorDrawable
-                drawable.setTintList(ColorStateList.valueOf(Color.BLACK))
-            }
-            selectedButton = null
-        }
-    }
-
-    // SingleTon
-    class MySingleton constructor(context: Context) {
-        companion object {
-            @Volatile
-            private var INSTANCE: MySingleton? = null
-            fun getInstance(context: Context) =
-                INSTANCE ?: synchronized(this) {
-                    INSTANCE ?: MySingleton(context).also {
-                        INSTANCE = it
-                    }
-                }
-        }
-
-        val requestQueue: RequestQueue by lazy {
-            // applicationContext is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-            Volley.newRequestQueue(context.applicationContext)
-        }
-        fun <T> addToRequestQueue(req: Request<T>) {
-            requestQueue.add(req)
-        }
-    }
-
-    override fun updateTextView(textView: TextView) {
-        if(selectedNumber != null) {
-            textView.text = selectedNumber
+        // need to set button text to view here;
+        if (view is Button) {
+            Toast.makeText(this, "${view.text} selected", Toast.LENGTH_SHORT).show()
+        } else { // image button
+            Toast.makeText(this, "Empty Cell selected", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/rcube/suduku/SudukuGridLayout.kt
+++ app/src/main/java/com/rcube/suduku/SudukuGridLayout.kt
@@ -1,5 +1,6 @@
 package com.rcube.suduku
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
@@ -12,85 +13,73 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.gridlayout.widget.GridLayout
 
-class SudokuGridLayout @JvmOverloads constructor(
+class SudukuGridLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : GridLayout(context, attrs, defStyle) {
 
-    private val rows = 9
-    private val columns = 9
+    private val gameSize = 9
+    private var cellSize = 0F
 
-    private var currCellWidth = 0
-    private var currCellHeight = 0
+    private val textViewMatrix: Array<Array<TextView?>> = Array(gameSize) { Array<TextView?>(gameSize) { null } }
 
-    private var selectedRow = -1
-    private var selectedCol = -1
+    @SuppressLint("DrawAllocation")
+    override fun onMeasure(widthSpec: Int, heightSpec: Int) {
+        super.onMeasure(widthSpec, heightSpec)
 
-    private val textViewMatrix = Array(9) { Array<TextView?>(9) { null } }
-    
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        cellSize = (measuredWidth / gameSize).toFloat()
 
-        val width = measuredWidth
-        val height = measuredHeight
+        val cellBorder = GradientDrawable()
+        cellBorder.setStroke(1, Color.parseColor("#CCCCCC"))
 
-        val cellWidth = width / columns
-        val cellHeight = height / rows
-
-        currCellWidth = cellWidth
-        currCellHeight = cellHeight
-
-        for (i in 0 until rows) {
+        for (i in 0 until gameSize) {
 
             when (i % 3) {
-                0, 3, 6 -> drawThickLines(i, "row")
+                0 -> drawThickLines(i, "row")
             }
 
-            for (j in 0 until columns) {
+            for (j in 0 until gameSize) {
                 val textView = TextView(context)
                 val params = LayoutParams()
-                params.width = cellWidth
-                params.height = cellWidth
+
+                params.width = cellSize.toInt()
+                params.height = cellSize.toInt()
                 params.rowSpec = spec(i, 1)
                 params.columnSpec = spec(j, 1)
 
                 textView.setTextAppearance(R.style.TableCell)
                 textView.gravity = Gravity.CENTER
                 textView.tag = "$i$j"
-                addView(textView, params)
-
-//                Add cellBorder
-                val cellBorder = GradientDrawable()
-                cellBorder.setStroke(1, Color.parseColor("#CCCCCC"))
                 textView.background = cellBorder
+                textView.setOnClickListener { setCellText(it) }
 
-                textViewMatrix[i][j] = textView
+                this.textViewMatrix[i][j] = textView
+                addView(textView, params)
             }
         }
 
-        for (j in 0 until columns) {
-
-            when (j % 3) {
-                0, 3, 6 -> drawThickLines(j, "col")
+        for (j in 0 until gameSize) {
+            when(j % 3) {
+                0 -> drawThickLines(j, "col")
             }
         }
     }
 
     private fun drawThickLines(num: Int, type: String) {
-        if (num != 0) {
+        if(num != 0) {
             val lineView = View(context)
             lineView.setBackgroundColor(Color.BLACK)
-            val lineParams = LayoutParams()
 
-            if (type == "row") {
-                lineParams.width = currCellWidth * 9
-                lineParams.height = 6
+            val lineParams = LayoutParams()
+            if(type == "row") {
+                lineParams.width = (cellSize * gameSize).toInt()
+                lineParams.height = 5
                 lineParams.rowSpec = spec(num, 1)
                 lineParams.columnSpec = spec(0, 9)
             } else {
-                lineParams.height = currCellWidth * 9
-                lineParams.width = 6
+                lineParams.height = (cellSize * gameSize).toInt()
+                lineParams.width = 5
                 lineParams.rowSpec = spec(0, 9)
                 lineParams.columnSpec = spec(num, 1)
             }
@@ -99,50 +88,26 @@ class SudokuGridLayout @JvmOverloads constructor(
         }
     }
 
-
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return when (event?.action) {
+        return when(event?.action) {
             MotionEvent.ACTION_DOWN -> {
                 handleTouchEvent(event.x, event.y)
                 true
-            }
-            else -> false
+            } else -> false
         }
     }
 
-    // Handle Touch Event
     private fun handleTouchEvent(x: Float, y: Float) {
-        val row = (y / currCellWidth).toInt()
-        val col = (x / currCellHeight).toInt()
+        val row = (y / cellSize).toInt()
+        val col = (x / cellSize).toInt()
 
-        selectedCol = col
-        selectedRow = row
+        textViewMatrix[row][col]?.callOnClick()
+        Log.d("SuDuKu_Game", "Row: $row, Col: $col")
+    }
 
-        Log.d("SuDuKU", "Row: $row, Col: $col")
-
-        val textView = textViewMatrix[row][col] as TextView
-//        if (textView!= null) {
-//            Log.d("SuDuKU", "Before update: ${textView.text}")
-//            (context as? OnCellTouch)?.updateTextView(textView)
-//            textView.invalidate()
-//            Log.d("SuDuKU", "After update: ${textView.text}")
-//
-//        }
-
-        textView.text = "";
-        textView.invalidate()
+    fun setCellText(view: View) {
+        val textView = view as TextView
         val randomNumber = (1..9).random()
         textView.text = randomNumber.toString()
-
-//        val textView = findViewWithTag<TextView>("$row$col")
-//        if (textView!= null) {
-//                textView.setBackgroundColor(Color.parseColor("#ecd4ee"))
-////            (context as? OnCellTouch)?.updateTextView(textView)
-//        }
     }
-
-    interface OnCellTouch {
-        fun updateTextView(textView: TextView)
-    }
-
 }
